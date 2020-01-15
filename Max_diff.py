061850aee1abb2209ba401df94a4f421d09db0e7
pasbahar <pasbahar@gmail.com>
2020-01-14 21:01:32 -0500

    changes

--- Max_diff.py
+++ Max_diff.py
@@ -1,11 +1,17 @@
 def maxdif(arr):
-    res,mn=-1,arr[0]
+    res,mn=-1,1000000
+    flag=0
     for i in arr:
         if mn>i:
             mn=i
+        else:
+            flag=1
         if res<i-mn:
             res=i-mn
-    return res
+    if flag:
+        return res
+    else:
+        return -1
 
 for i in range(int(input())):
     n=int(input())
